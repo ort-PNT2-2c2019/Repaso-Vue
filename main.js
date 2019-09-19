1570c1bad5f3a59b9037c02ebe1f462f5765c726
Sofia Ferragut <sofia.ferragut@gmail.com>
2019-09-19 12:09:57 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,17 @@
+new Vue({
+    el: '#editable',
+    data: {
+       
+    }
+})
+
+new Vue({
+    el: '#reverse',
+    data: {
+       
+    },
+    methods: {
+        // REVERSA: this.text.split('').reverse().join('')
+       
+    }
+})
